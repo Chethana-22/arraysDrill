fae8e8db9457f9f6855bb88b7aa3f0307cb6d168
Chethana <chethanasv22@gmail.com>
2023-10-09 11:00:50 +0530

    updated find method

--- find.js
+++ find.js
@@ -2,10 +2,9 @@ export function find(elements, cb){
     const result = []
     for(let index=0;index<elements.length;index++){
         if(cb(elements[index])){
-            result.push(true);
+            return true;
         }else{
-            result.push(undefined);
+            return undefined;
         }
     }  
-    return result; 
 }
--- testFind.js
+++ testFind.js
@@ -1,7 +1,8 @@
 import { find } from "./find.js";
 
 function cb(element){
-    return element % 2 !== 0;
+    return element % 2 !== 0;    // returns true
+    // return element === 0;     // returns undefined
 }
 
 const items = [1, 2, 3, 4, 5, 5];
