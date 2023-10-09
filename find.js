3a527f9134992b8247407853908e84bc828542d3
Chethana <chethanasv22@gmail.com>
2023-10-09 11:08:25 +0530

    updated find function

--- find.js
+++ find.js
@@ -2,8 +2,7 @@ export function find(elements, cb){
     for(let index=0;index<elements.length;index++){
         if(cb(elements[index])){
             return true;
-        }else{
-            return undefined;
         }
-    }  
+    }
+    return false;
 }
